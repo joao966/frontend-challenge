2c1b675e514d20b9cec0f96c039312a3260cfce4
joao966 <joaooliveira966@gmail.com>
2023-02-22 18:57:11 -0300

    FIX: ajusta css global

--- front/src/style/global.ts
+++ front/src/style/global.ts
@@ -27,12 +27,15 @@ const GlobalStyle = createGlobalStyle`
 
 
   * {
-    font-family: Poppins-normal;
-    width: 100%;
-    height: 100vh;
     margin: 0;
     padding: 0;
+    outline: 0;
     box-sizing: border-box;
+  }
+  
+  body {
+    font-family: Poppins-normal;
+    background-color: #808080;
   }
 `;
 
