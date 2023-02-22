b6b5c38160c2e15912ee7bf96e6c3b365ce95e47
joao966 <joaooliveira966@gmail.com>
2023-02-22 04:11:27 -0300

    ADD: cria theme

---
+++ front/src/style/theme.ts
@@ -0,0 +1,15 @@
+const theme = {
+  colors: {
+    primary: '#0070f3',
+    secondary: '#333',
+    error: '#ff0000',
+    success: '#008000',
+  },
+  fontSizes: {
+    small: '12px',
+    medium: '16px',
+    large: '24px',
+  },
+};
+
+export default theme;
