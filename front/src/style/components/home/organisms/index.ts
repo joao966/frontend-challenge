8d4afb02a2dfbdadebed3f9a35d73b33cc99cd2b
joao966 <joaooliveira966@gmail.com>
2023-02-22 22:19:44 -0300

    ADD: cria styled components

--- front/src/style/components/home/organisms/index.ts
+++ front/src/style/components/home/organisms/index.ts
@@ -1,8 +1,33 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  max-width: 978px;
-  margin: 0 auto;
-  padding: 0 20px;
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  row-gap: 16px;
+
+  a {
+    text-decoration: none;
+  }
+`;
+
+export const Button = styled.div`
+  width: 272px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #363636;
+  color: #FFFF;
+  border-radius: 4px;
+  padding: 5px;
+  cursor: pointer;
+  font-style: italic;
+
+  @media (max-width: 571px) {
+    width: 150px;
+  }
 `;
 
