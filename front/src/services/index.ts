cffafec95d619afd13aebd0d81c1509fd84d8364
joao966 <joaooliveira966@gmail.com>
2023-02-27 05:43:17 -0300

    ADD: cria serviço para listar produtos

---
+++ front/src/services/index.ts
@@ -0,0 +1,13 @@
+export function getProducts() {
+  return new Promise(async(resolve, reject) => {
+    try {
+      const response = await fetch('http://localhost:3333/graphql?query={allProducts{name,description,image_url}}');
+      const { data: { allProducts } } = await response.json();
+      resolve(allProducts);
+      return;
+    } catch (error) {
+      reject(new Error('Erro ao trazer produtos!'));
+      return;
+    }
+  });
+};
