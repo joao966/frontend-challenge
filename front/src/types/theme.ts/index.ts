e574d076d507dc4c71a370d13d1297239859c63b
joao966 <joaooliveira966@gmail.com>
2023-02-27 01:30:43 -0300

    ADD: adiciona tipos em home

---
+++ front/src/types/theme.ts/index.ts
@@ -0,0 +1,13 @@
+export type themeType = {
+  colors: {
+      primary: string;
+      secondary: string;
+      error: string;
+      success: string;
+  };
+  fontSizes: {
+      small: string;
+      medium: string;
+      large: string;
+  }
+}
