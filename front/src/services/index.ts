2c381c69e4179ff809085fa9a49a4c992c1a1a68
joao966 <joaooliveira966@gmail.com>
2023-03-01 00:48:26 -0300

    ADD: cria typos para função de listar produtos

--- front/src/services/index.ts
+++ front/src/services/index.ts
@@ -1,4 +1,6 @@
-export function getProducts() {
+import { listProduct } from "@/types/catalog";
+
+export function getProducts(): Promise<listProduct[]> {
   return new Promise(async(resolve, reject) => {
     try {
       const response = await fetch('http://localhost:3333/graphql?query={allProducts{name,description,image_url}}');
